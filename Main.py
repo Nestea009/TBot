c607e055f464e7113f6d027bcd5306bf4e5b8b9d
Néstor <nestorblancotech@gmail.com>
2023-05-22 18:20:56 +0200

    Con markox uwu

--- Main.py
+++ Main.py
@@ -2,24 +2,27 @@ import os
 from multiprocessing import Process
 
 def script1():
-    os.system("RealMain.py")     
+  os.system("python ./RealMain.py")
+
 def script2():
-    os.system("H.py") 
+  os.system("python ./H.py")
+
 def script3():
-    os.system("L.py") 
+  os.system("python ./L.py")
+
 def script4():
-    os.system("Server_status.py") 
+  os.system("python ./Server_status.py")
 
 if __name__ == '__main__':
-    p = Process(target=script1)
-    q = Process(target=script2)
-    s = Process(target=script3)
-    t = Process(target=script4)
-    p.start()
-    q.start()
-    s.start()
-    t.start()
-    p.join()
-    q.join()
-    s.join()
-    t.join()
+  p = Process(target=script1)
+  q = Process(target=script2)
+  s = Process(target=script3)
+  t = Process(target=script4)
+  p.start()
+  q.start()
+  s.start()
+  t.start()
+  p.join()
+  q.join()
+  s.join()
+  t.join()
